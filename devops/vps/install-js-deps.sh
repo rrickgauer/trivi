21cb0a331a947f81661b74a4b92e9d6d27941657
Ryan Rickgauer <rrickgauer1@gmail.com>
2024-07-23 20:43:43 -0500

    upload to vps

---
+++ devops/vps/install-js-deps.sh
@@ -0,0 +1,6 @@
+npm install tslib --save-dev
+npm install typescript --save-dev
+npm install @rollup/plugin-typescript --save-dev
+npm install @types/bootstrap --save-dev 
+npm install nanoid@3 --save-dev
+npm install @rollup/plugin-node-resolve --save-dev 
